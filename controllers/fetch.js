803a6f02057f01c25a7141094c830e786b6a3f4e
Ben French <ben.jminw@gmail.com>
2018-03-23 08:25:11 -0600

    Scraping articles route.

--- controllers/fetch.js
+++ controllers/fetch.js
@@ -5,12 +5,34 @@ var cheerio = require('cheerio');
 var request = require('request');
 
 exports.scrape_get = function (req, res) {
-    request('http://www.google.com', function (error, response, body) {
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body); // Print the HTML for the Google homepage.
+    request('https://techcrunch.com', function (error, response, body) {
+        var $ = cheerio.load(body);
+
+        var results = [];
+        // Now, we grab every h2 within an article tag, and do the following:
+        $(".post-block").each(function (i, element) {
+            // Save an empty result object
+            var result = {};
+
+            // Add the text and href of every link, and save them as properties of the result object
+            result.title = $(this).find(".post-block__title__link").text().trim();
+
+            result.url = $(this).find(".post-block__title__link").attr("href").trim();
+
+            result.summary = $(this).find(".post-block__content").text().trim();
+
+            results.push(result);
+            
+        });
+        console.log(`Scraped ${results.length} articles.`);
+
+        Headline.create(results)
+        .then( dbHeadline => {
+            console.log(`Added ${dbHeadline.length} articles to DB.`);
+            res.status(201).end();
+        })
+        .catch( err => {
+            res.status(500).end();
+        });
     });
-
-    res.end();
-
 };
